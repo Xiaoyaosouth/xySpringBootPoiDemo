6e82eb520913dfa8059a7019757de04c9f5933d2
xiaoyao <626753724@qq.com>
2020-05-08 12:31:10 +0800

    更新controller

--- src/main/java/com/fe/mysbpoidemo/controller/DynamicExcelReaderController.java
+++ src/main/java/com/fe/mysbpoidemo/controller/DynamicExcelReaderController.java
@@ -22,7 +22,8 @@ public class DynamicExcelReaderController {
     public List<Object> getMyDynamicExcelData(String fileUrl) {
         System.out.println("-------获取动态生成的表格数据-------");
         DynamicExcelDataService myService = new DynamicExcelDataService();
-        List<Object> data = myService.getDynamicModelList(fileUrl);
+        // 使用新方法，处理中间空列空行无法取值的问题 2020-05-08
+        List<Object> data = myService.getDynamicModelListV2(fileUrl);
         return data;
     }
 }
